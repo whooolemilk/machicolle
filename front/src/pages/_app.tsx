bda2500da0564c1fdb18812f594891c9371a7bb2
Daiki Maekawa <67625825+Daaiki@users.noreply.github.com>
2023-01-29 23:11:42 +0900

    カラーテーマ追加，リセットCSS導入 (#22)

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { store } from '@/rtk/store'
 import { Provider } from 'react-redux'
+import 'normalize.css'
+import '@/styles/global.scss'
 import '@/lib/firebase'
 
 export default function App({ Component, pageProps }: AppProps) {
