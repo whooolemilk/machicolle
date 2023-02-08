0e16cdf22ca8cc8155b1b288e1609a1eb9712991
eve00 <64850924+eve00@users.noreply.github.com>
2023-02-08 15:00:55 +0900

    修正

--- front/src/hooks/useGetSpotData.ts
+++ front/src/hooks/useGetSpotData.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { LatLng } from '@/components/SpotSearchBox'
+import { LatLng } from '@/components/Forms/FormSearchSpot'
 
 export type spotDataType = {
   name: string | undefined
